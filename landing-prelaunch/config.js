63f9df2b20468f6ffbd9269b644d0bcf6b8f7810
medconn <medconnect.contacto@gmail.com>
2026-03-27 22:22:41 -0300

    Remove prelaunch landing from main app repo

--- landing-prelaunch/config.js
+++
@@ -1,9 +0,0 @@
-window.KLINIP_PRELAUNCH_CONFIG = {
-  appUrl: "https://www.klinip.cl",
-  waitlistUrl: "",
-  paymentUrl: "",
-  plusPlanUrl: "",
-  familyPlanUrl: "",
-  whatsappUrl: "",
-  emailUrl: "mailto:contacto@klinip.cl",
-};
